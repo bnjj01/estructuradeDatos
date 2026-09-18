17a902619959fa34bba6f690f7b78c31f073b22d
bepoamhu.2016@gmail.com <todmy8@gmail.com>
2026-09-18 00:24:43 -0300

    tp2 ej 2

--- src/main/java/com/ed/lewischase/list/OrderedList.java
+++ src/main/java/com/ed/lewischase/list/OrderedList.java
@@ -6,6 +6,11 @@ public class OrderedList<T> implements OrderedListADT<T> {
     LinearNode<T> front, rear;
     int count;
 
+    public OrderedList() {
+        front = rear = null;
+        count = 0;
+    }
+
     @Override
     public void add(T element) {
         LinearNode<T> newNode = new LinearNode<>(element);
@@ -46,7 +51,7 @@ public class OrderedList<T> implements OrderedListADT<T> {
 
         if(isEmpty()) return false;
 
-        while(current.getNext() != null){
+        while(current != null){
             if(elemComp.compareTo(current.getElement()) == 0){
                 return true;
             }
@@ -57,50 +62,97 @@ public class OrderedList<T> implements OrderedListADT<T> {
 
     @Override
     public T first() throws EmptyListException {
-        // TODO Auto-generated method stub
-        return null;
+        if(isEmpty()) throw new EmptyListException();
+        return front.getElement();
     }
 
     @Override
     public boolean isEmpty() {
-        // TODO Auto-generated method stub
+        if(count == 0) return true;
         return false;
     }
 
     @Override
     public Iterator<T> iterator() {
-        // TODO Auto-generated method stub
-        return null;
+        return new OrderedListIterator<>(front);
     }
 
     @Override
     public T last() throws EmptyListException {
-        // TODO Auto-generated method stub
-        return null;
+        if(isEmpty()) throw new EmptyListException();
+        return rear.getElement();
     }
 
     @Override
     public T remove(T element) throws EmptyListException, ElementNotFoundException {
-        // TODO Auto-generated method stub
-        return null;
+        if(isEmpty()) throw new EmptyListException();
+        Comparable<T> elemComp = (Comparable<T>)element;
+        LinearNode<T> current = front;
+        LinearNode<T> previous = null;
+
+        while(current != null && elemComp.compareTo(current.getElement()) != 0){
+            previous = current;
+            current = current.getNext();
+        }
+
+        if(current == null){throw new ElementNotFoundException(element.toString());}
+
+        T copy = current.getElement();
+        
+        if(size() == 1){
+            front = rear = null;
+        }else if(current == front){
+            front = front.getNext();
+        }else if(current == rear){
+            rear = previous;
+            rear.setNext(null);
+        }else{
+            previous.setNext(current.getNext());
+        }
+        count--;
+        return copy;
     }
 
     @Override
     public T removeFirst() throws EmptyListException {
-        // TODO Auto-generated method stub
-        return null;
+        if(isEmpty()) throw new EmptyListException();
+        T copy = front.getElement();
+        if(count == 1){
+            front = rear = null;
+        }else{
+            front = front.getNext();
+        }
+        count--;
+        return copy;
     }
 
     @Override
     public T removeLast() throws EmptyListException {
-        // TODO Auto-generated method stub
-        return null;
+        if(isEmpty())throw new EmptyListException();
+        T copy = rear.getElement();
+        if(count == 1){
+            front = rear= null;
+            count--;
+            return copy;
+        }
+
+        LinearNode<T> current = front;
+        LinearNode<T> previous = null;
+
+        while(current != rear){
+            previous = current;
+            current = current.getNext();
+        }
+        
+        rear = previous;
+        rear.setNext(null);
+        count--;
+        return copy;
     }
 
     @Override
     public int size() {
-        // TODO Auto-generated method stub
-        return 0;
+        return count;
     }
     
 }
